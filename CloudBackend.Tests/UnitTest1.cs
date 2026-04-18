b5bb884fa4ca80c62a2b5b2130ad68190fe359ed
Igor Kotlik <wrx89196@student.wroclaw.merito.pl>
2026-04-18 15:13:32 +0200

    Add tests

---
+++ CloudBackend.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace CloudBackend.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void NewTask_ShouldNotBeCompleted()
+    {
+        var task = new CloudBackend.Models.TaskItem();
+        task.Title = "Przetestowac bezpiecznik";
+        Assert.False(task.IsCompleted);
+    }
+}
